49bd89899dc3938b341c4a17aa8733d5a5d41858
farahalsubhi30-creator <farah.alsubhi30@gmail.com>
2026-03-03 22:08:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+
 function showWelcomeMessage() {
   alert("مرحباً بك في مدينة أملج!");
   document.body.style.backgroundColor = "#E6F7FF";
