bc8c325c3e9f7ce0e14dd697ab21942a71a13181
farahalsubhi30-creator <farah.alsubhi30@gmail.com>
2026-03-03 21:33:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+
+function showWelcomeMessage() {
+  alert("مرحباً بك في موقع مدينتي!");
+}
