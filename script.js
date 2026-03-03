63512dd50c12743533f06d16b1f94070d58e9520
farahalsubhi30-creator <farah.alsubhi30@gmail.com>
2026-03-03 21:37:41 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const navLinks = document.getElementById('navLinks');
-  navLinks.classList.toggle('active');
-}
-
-function showWelcomeMessage() {
-  alert("مرحباً بك في موقع مدينتي!");
-}
